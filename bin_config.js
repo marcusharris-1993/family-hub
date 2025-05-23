834de785c3682d04c9ea32a98fc2a2c4c4be740f
marcusharris-1993 <marcus.harris1993@gmail.com>
2025-05-23 21:42:28 +0100

    Update bin_config.js

--- bin_config.js
+++ bin_config.js
@@ -1,9 +1,28 @@
 const binConfig = {
+  maxUpcoming: 3,
   icons: {
-    green: './green_bin.png',
-    black: './black_bin.png',
-    brown: './brown_bin.png'
+    green: "green_bin.png",
+    black: "black_bin.png",
+    brown: "brown_bin.png"
   },
-  title: 'Bin Collection Schedule',
-  maxUpcoming: 5
+  icsText: `
+BEGIN:VCALENDAR
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+PRODID://FixMyStreet//Bin Collection Calendars//EN
+BEGIN:VEVENT
+DTSTART;VALUE=DATE:20250523
+SUMMARY:Empty Bin 240L Green
+END:VEVENT
+BEGIN:VEVENT
+DTSTART;VALUE=DATE:20250530
+SUMMARY:Empty Bin 240L Black
+END:VEVENT
+BEGIN:VEVENT
+DTSTART;VALUE=DATE:20250530
+SUMMARY:Empty Bin 240L Brown
+END:VEVENT
+END:VCALENDAR
+  `.trim()
 };
