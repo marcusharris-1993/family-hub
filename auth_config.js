ceb88e7b21da9944eb2c258a3a4fab3fdf91e00c
marcusharris-1993 <marcus.harris1993@gmail.com>
2025-05-26 20:45:57 +0100

    Update auth_config.js

--- auth_config.js
+++ auth_config.js
@@ -1,5 +1,5 @@
 const authConfig = {
-  clientId: "YOUR_CLIENT_ID",
+  clientId: "937015904322-2ukhcpv7309fcjkafhvor678t8d10mu6.apps.googleusercontent.com",
   scopes: [
     "https://www.googleapis.com/auth/calendar.readonly",
     "https://www.googleapis.com/auth/drive.readonly"
