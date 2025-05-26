a01b2a223ac59f7cf37c47a6f9368eef0300df7e
marcusharris-1993 <marcus.harris1993@gmail.com>
2025-05-26 21:12:38 +0100

    Update auth_config.js

--- auth_config.js
+++ auth_config.js
@@ -1,7 +1,8 @@
-const authConfig = {
-  clientId: "937015904322-2ukhcpv7309fcjkafhvor678t8d10mu6.apps.googleusercontent.com",
-  scopes: [
-    "https://www.googleapis.com/auth/calendar.readonly",
-    "https://www.googleapis.com/auth/drive.readonly"
-  ]
-};
+function gapiInit() {
+  gapi.load("client:auth2", () => {
+    gapi.client.init({
+      clientId: "937015904322-2ukhcpv7309fcjkafhvor678t8d10mu6.apps.googleusercontent.com",
+      scope: "https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/drive.readonly"
+    });
+  });
+}
